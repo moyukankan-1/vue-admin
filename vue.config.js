c190be487e49fddc198589a4e5f151b80f2ebcdf
wang <moyukankan@126.com>
2020-04-26 16:58:46 +0800

    vue-admin

---
+++ vue.config.js
@@ -0,0 +1,60 @@
+const path = require('path')
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
+
+  // 将构建好的文件输出到哪里
+  outputDir: process.env.NODE_ENV === 'production' ? 'dist' : 'devdist',
+
+  // 是否在保存的时候使用 `eslint-loader` 进行检查。
+  // 有效的值：`ture` | `false` | `"error"`
+  // 当设置为 `"error"` 时，检查出的错误会触发编译失败。
+  lintOnSave: true,
+
+  // 使用带有浏览器内编译器的完整构建版本
+  // 查阅 https://cn.vuejs.org/v2/guide/installation.html#运行时-编译器-vs-只包含运行时
+  // compiler: false,
+
+  // 是否为生产环境构建生成 source map？
+  productionSourceMap: false,
+
+  // 调整内部的 webpack 配置。
+  // 查阅 https://github.com/vuejs/vue-docs-zh-cn/blob/master/vue-cli/webpack.md
+  chainWebpack: (config) => {},
+  configureWebpack: (config) => {},
+
+  // CSS 相关选项
+  css: {
+      // 将组件内的 CSS 提取到一个单独的 CSS 文件 (只用在生产环境中)
+      // 也可以是一个传递给 `extract-text-webpack-plugin` 的选项对象
+      extract: true,
+
+      // 是否开启 CSS source map？
+      sourceMap: false,
+
+      // 为预处理器的 loader 传递自定义选项。比如传递给
+      // sass-loader 时，使用 `{ sass: { ... } }`。
+      loaderOptions: {
+        sass: {
+          prependData: `@import "./src/styles/main.scss";`
+        }
+      },
+
+      // 为所有的 CSS 及其预处理文件开启 CSS Modules。
+      // 这个选项不会影响 `*.vue` 文件。
+      modules: false
+  },
+
+  // 在生产环境下为 Babel 和 TypeScript 使用 `thread-loader`
+  // 在多核机器下会默认开启。
+  parallel: require('os').cpus().length > 1,
+
+  // PWA 插件的选项。
+  // 查阅 https://github.com/vuejs/vue-docs-zh-cn/blob/master/vue-cli-plugin-pwa/README.md
+  pwa: {},
+
+  // 第三方插件的选项
+  pluginOptions: {
+
+  }
+}
