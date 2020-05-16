e19d8ff3d3f18cf3e9ddfab28be13dde536dd9f0
915171176@qq.com <moyukankan@126.com>
2020-05-16 09:13:59 +0800

    编辑详情

--- src/views/info/index.vue
+++ src/views/info/index.vue
@@ -62,7 +62,7 @@
     <!--表格-->
     <el-table :data="tableData.item" border style="width: 100%" @selection-change="handleSelectionChange" v-loading="loadingData">
       <el-table-column type="selection" width="55"></el-table-column>
-      <el-table-column prop="title" label="标题" width="830"></el-table-column>
+      <el-table-column prop="title" label="标题" width="750"></el-table-column>
       <el-table-column prop="categoryId" label="类型" width="130" :formatter="toCate"></el-table-column>
       <el-table-column prop="createDate" label="日期" width="235" :formatter="toData"></el-table-column>
       <el-table-column prop="user" label="管理员" width="115"></el-table-column>
@@ -70,6 +70,7 @@
         <template slot-scope="scope">
           <el-button type="danger" size="mini" @click="deleteItem(scope.row.id)">删除</el-button>
           <el-button type="success" size="mini" @click="editInfo(scope.row.id)">编辑</el-button>
+          <el-button type="success" size="mini" @click="editInfo(scope.row.id)">编辑详情</el-button>
         </template>
       </el-table-column>
     </el-table>
