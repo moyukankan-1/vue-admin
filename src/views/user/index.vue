c5b54f11372e156fc43115cb630b45791772989e
915171176@qq.com <moyukankan@126.com>
2020-05-20 11:44:07 +0800

    信息管理模块

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,12 +1,53 @@
 <template>
-  <div>用户列表</div>
+  <div>
+    <el-row :gutter="20">
+      <el-col :span='20'>
+        <div class="label-warp">
+          <label>关键字</label>
+          <div class="warp-content">
+            <el-row :gutter="20">
+              <el-col :span="3">
+                <el-select v-model="data.selectValue">
+                  <el-option v-for="item in data.option" :key="item.value" :value="item.value" :label="item.label"></el-option>
+                </el-select>
+              </el-col>
+              <el-col :span='4'>
+                <el-input placeholder="请输入内容"></el-input>
+              </el-col>
+              <el-col :span="15">
+                <el-button type="danger">搜索</el-button>
+              </el-col>
+            </el-row>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span='4'>
+        <el-button type='danger' class="pull-right">添加用户</el-button>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 <script>
+import { reactive } from '@vue/composition-api'
 export default {
   setup(props) {
-    
+    const data = reactive({
+      selectValue: '',
+      option: [
+        { value: 'name', label: '姓名'},
+        { value: 'phone', label: '手机号'},
+        { value: 'email', label: '邮箱'}
+      ]
+    })
+    return {
+      data
+    }
   }
 }
 </script>
 <style lang="scss" scoped>
+@import '@/styles/config.scss';
+.label-warp {
+  @include labelDom(left,60,40)
+}
 </style>
