8d3c1a4b6972136d86a43cc3ad4d3c9092f48bca
915171176@qq.com <moyukankan@126.com>
2020-06-05 09:31:07 +0800

    打包部署

--- vue.config.js
+++ vue.config.js
@@ -75,29 +75,29 @@ module.exports = {
   // 查阅 https://github.com/vuejs/vue-docs-zh-cn/blob/master/vue-cli-plugin-pwa/README.md
   pwa: {},
 
-  devServer: {
-    open: false, // 编译完成是否打开网页
-    host: '0.0.0.0', // 指定使用地址，默认localhost,0.0.0.0代表可以被外界访问
-    port: 8080, // 访问端口
-    https: false, // 编译失败时刷新页面
-    hot: true, // 开启热加载
-    hotOnly: false,
-    proxy: {
-      '/devApi': {
-          target: "http://www.web-jshtml.cn/productapi/token",
-          changeOrigin: true,
-          pathRewrite: {
-              '^/devApi': ''
-          }
-      }
-    },
-    overlay: { // 全屏模式下是否显示脚本错误
-      warnings: true,
-      errors: true
-    },
-    before: app => {
-    }
-  },
+  // devServer: {
+  //   open: false, // 编译完成是否打开网页
+  //   host: '0.0.0.0', // 指定使用地址，默认localhost,0.0.0.0代表可以被外界访问
+  //   port: 8080, // 访问端口
+  //   https: false, // 编译失败时刷新页面
+  //   hot: true, // 开启热加载
+  //   hotOnly: false,
+  //   proxy: {
+  //     '/devApi': {
+  //         target: "http://www.web-jshtml.cn/productapi/token",
+  //         changeOrigin: true,
+  //         pathRewrite: {
+  //             '^/devApi': ''
+  //         }
+  //     }
+  //   },
+  //   overlay: { // 全屏模式下是否显示脚本错误
+  //     warnings: true,
+  //     errors: true
+  //   },
+  //   before: app => {
+  //   }
+  // },
 
   // 第三方插件的选项
   pluginOptions: {
